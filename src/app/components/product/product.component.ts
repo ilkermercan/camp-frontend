1b20db8166e387e68b85668e0c9b63eaf82ddc3e
İlker Mercan <ilkermercan@outlook.com>
2021-03-24 23:43:57 +0300

    BITIS

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
+  filterText = '';
+
+  constructor(
+    private productService: ProductService,
+    private activatedRouted: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService:CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRouted.params.subscribe((params) => {
+      if (params['categoryId']) {
+        this.getProductByCategory(params['categoryId']);
+      } else {
+        this.getProducts();
+      }
+    });
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
+
+  getProductByCategory(categoryId: number) {
+    this.productService
+      .getProductsByCategory(categoryId)
+      .subscribe((response) => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      });
+  }
+
+  addToCart(product: Product) {
+    this.toastrService.success("Sepete eklendi", product.productName);
+    this.cartService.addToCart(product);
+  }
+}
